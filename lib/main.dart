2e74a715c274301bcd408a2a297664fc530ff35f
LUIS_BRANDON_CRISTOBAL_RAMIREZ <a.21308051280456@cbtis128.edu.mx>
2024-05-06 16:40:38 +0000

    ans

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,60 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // useMaterial3: false,
         primarySwatch: Colors.blue,
       ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
 
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+        title: const Text(
+          'CFE_0456',
+          style: TextStyle(color: Colors.white),
         ),
+        backgroundColor: const Color(0xff139729),
+      ),
+      body: Image.asset(
+        "assets/images/icon.png",
+        width: 400,
+        height: 300,
+        fit: BoxFit.contain,
+        frameBuilder: (context, child, frame, loaded) {
+          if (loaded) {
+            return child;
+          }
+          return AnimatedOpacity(
+            opacity: frame == null ? 0 : 1,
+            duration: const Duration(seconds: 1),
+            curve: Curves.easeOut,
+            child: child,
+          );
+        },
+        errorBuilder: (context, exception, stackTrace) {
+          return const Center(
+            child: Text("Chale no sirve, No se puedo"),
+          );
+        },
       ),
     );
   }
